cabc4ae82c3790c26972e9dab2d7d8ec7685c0f1
pataruco <pataruco@gmail.com>
2016-03-29 10:36:03 +0100

    Add method to get character

--- models/letter-list.js
+++ models/letter-list.js
@@ -2,4 +2,11 @@ var LetterList = function(){
   this.list = null;
 };
 
+LetterList.prototype.init = function (listData) {
+  this.list = listData;
+};
+
+LetterList.prototype.character = function () {
+  return this.list.atoz_programmes.character;
+};
 module.exports = LetterList;
