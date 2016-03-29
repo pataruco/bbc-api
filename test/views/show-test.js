3348e9fdd33fd24a9fed081bcc916d1389809f48
pataruco <pataruco@gmail.com>
2016-03-29 11:43:57 +0100

    Test if next page is rendered

--- test/views/show-test.js
+++ test/views/show-test.js
@@ -35,7 +35,7 @@ describe("View", () => {
         res.text.should.not.match(/Page 2/);
         res.text.should.match(/Letter A/);
         done();
-      })
+      });
     });
 
     it ("should not render a Letter B Page 1 page", (done)=>{
@@ -49,8 +49,21 @@ describe("View", () => {
         res.text.should.match(/Page 1/);
         res.text.should.not.match(/Letter B/);
         done();
-      })
+      });
     });
+    
+    it ("should render a next page link", (done)=>{
+          api
+          .get(letterAPageOne)
+          .set("Accept", "application/json")
+          .expect(200)
+          .expect('Content-Type', /html/)
+          .end(function(err, res) {
+            should.not.exist(err);
+            res.text.should.match(/Next/);
+            done();
+          })
+        });
 
   });
 });
