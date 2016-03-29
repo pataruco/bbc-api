20a67d9861deb466f5c0dd5b55c6051246d21c66
pataruco <pataruco@gmail.com>
2016-03-29 11:32:17 +0100

    Test if Letter A and Page 1 is rendered in a page

---
+++ test/views/show-test.js
@@ -0,0 +1,27 @@
+var should        = require("chai").should(),
+    expect        = require("chai").expect,
+    supertest     = require("supertest"),
+    api           = supertest("http://localhost:3000"),
+    navData       = ("./nav-data")
+
+
+describe("View", () => {
+
+  describe("letter=a/page=1", () =>{
+    var letterAPageOne = "/letter=a/page=1"
+
+    it ("should render a Letter A Page 1 page", (done)=>{
+      api
+      .get(letterAPageOne)
+      .set("Accept", "application/json")
+      .expect(200)
+      .expect('Content-Type', /html/)
+      .end(function(err, res) {
+        should.not.exist(err);
+        res.text.should.match(/Page 1/);
+        res.text.should.match(/Letter A/);
+        done();
+      });
+    });
+  });
+});
