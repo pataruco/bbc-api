f51689ff6c6fb32e7e1e4efc356ffb98fff0af60
pataruco <pataruco@gmail.com>
2016-03-29 08:46:53 +0100

    Test the API request
    It respond 200 OK when is requested

---
+++ test/controllers/request-test.js
@@ -0,0 +1,16 @@
+var should        = require("chai").should(),
+    expect        = require("chai").expect,
+    supertest     = require("supertest"),
+    api           = supertest("https://ibl.api.bbci.co.uk");
+
+describe('Request to BBC API', () => {
+  var letterAPageOne = '/ibl/v1/atoz/a/programmes?page=1'
+
+  it("should return a 200 response", (done) => {
+    api
+      .get(letterAPageOne)
+      .set("Accept", "application/json")
+      .expect(200)
+      done();
+  });
+}); //end of BBC API
